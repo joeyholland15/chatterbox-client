20a31a4cc97e1c1ececa12c6cf2c37102cf27712
joeyholland15 <joeyholland15@gmail.com>
2015-12-07 19:14:07 -0800

    Updated chat feed so messages appear

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,8 +1,8 @@
 // YOUR CODE HERE:
 var message = {
   username: 'shawndrost',
-  text: 'TEEEXXTTT',
-  roomname: '4chan'
+  text: '<>&&',
+  roomname: 'Dima and Joey'
 };
 
 var app = {}; 
@@ -10,6 +10,7 @@ var app = {};
 var messages = 0;
 
 app.escapeHTML = function (text) {
+  if (text === undefined){return};
   var map = {
     '&': '&amp;',
     '<': '&lt;',
@@ -28,19 +29,19 @@ app.init = function() {
 
 app.send = function(message) {
   $.ajax({
-    // This is the url you should use to communicate with the parse API server.
-    url: app.server,
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log(message);
-    },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message');
-    }
-  });
+  // This is the url you should use to communicate with the parse API server.
+  url: app.server,
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message');
+  }
+});
 }; 
 
 app.fetch = function() {
@@ -55,15 +56,22 @@ app.fetch = function() {
       var chatRooms = {}; 
       var arrayOfMessageObjects = data.results;
  
-      _.each(arrayOfMessageObjects, function(messageObject){
-        app.escapeHTML(messageObject['text']); 
-        if(chatRooms[messageObject['roomname']] === undefined){
-          chatRooms[messageObject['roomname']] = [];
-          chatRooms[messageObject['roomname']].push(messageObject);
-        } else {
-          chatRooms[messageObject['roomname']].push(messageObject);
-        }
-      });
+      // _.each(arrayOfMessageObjects, function(messageObject){
+      //   messageObject['text'] = app.escapeHTML(messageObject['text']); 
+      //   if(chatRooms[messageObject['roomname']] === undefined){
+      //     chatRooms[messageObject['roomname']] = [];
+      //     chatRooms[messageObject['roomname']].push(messageObject);
+      //   } else {
+      //     chatRooms[messageObject['roomname']].push(messageObject);
+      //   }
+
+      // });
+
+      $.each(arrayOfMessageObjects, function(index, val) {
+        val['text'] = app.escapeHTML(val['text']); 
+        $('#chats').append("<li class = 'chat'>"+val.text+"</li>")
+      }); 
+      // console.log(arrayOfMessageObjects); 
       
     },
     error: function (data) {
@@ -74,15 +82,22 @@ app.fetch = function() {
 
 }; 
 
-app.clearMessages = function() {};
+//app.clearMessages = function() {};
 app.addMessage = function() {};
 app.addRoom = function() {}; 
 
 //<------------------------------------------------------------------------------------------->
+app.clearMessages = function(){
+  $('#chats').empty();
+  // app.fetch();
+
+}
+
 
 app.init();
+app.send(message);
 app.fetch = app.fetch.bind(app);
-setInterval(app.fetch, 2000);
+//setInterval(clear, 2000);
 
 
 
